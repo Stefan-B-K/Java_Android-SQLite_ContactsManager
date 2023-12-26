22fa6f5cd499e6b7cfcdfc0f00d5645c2540c3f3
iStef <stefan_b_k@icloud.com>
2023-12-26 18:42:22 +0200

    Add RecyclerView (list view)

--- app/src/main/java/com/istef/sqlite_contactmanager/MainActivity.java
+++ app/src/main/java/com/istef/sqlite_contactmanager/MainActivity.java
@@ -1,39 +1,43 @@
 package com.istef.sqlite_contactmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.istef.sqlite_contactmanager.adapter.MainRecViewAdapter;
 import com.istef.sqlite_contactmanager.data.DBManager;
 import com.istef.sqlite_contactmanager.model.Contact;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
+    private List<Contact> contactList;
+    private RecyclerView recyclerView;
+    private MainRecViewAdapter mainRecViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.recycleView);
+        recyclerView.setHasFixedSize(true);                                 //      !!!
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         try (DBManager dbManager = new DBManager(this)) {
-//            Contact testContact = new Contact("Emil", "00001110000000");
-//            long newId = dbManager.addContact(testContact);
-//            Log.d("ADD NEW", "onCreate: " + newId);
 
-//            Contact testContact = dbManager.getContact(1);
-//            testContact.setName("Madonna");
-//            testContact.setPhoneNumber("000 000 000");
-//            dbManager.updateContact(testContact);
+            contactList = dbManager.getAllContacts();
+            mainRecViewAdapter = new MainRecViewAdapter(this, contactList);
+            recyclerView.setAdapter(mainRecViewAdapter);
 
-//            Contact testContact = dbManager.getContact(2);
-//            dbManager.deleteContact(testContact);
-
-//            dbManager.dropTable(Config.DB_TABLE_NAME);
-            List<Contact> contactList = dbManager.getAllContacts();
-            Log.d("CONTACTS", "::::: " + contactList.toString());
-            Log.d("CONTACT COUNT", "::::: " + dbManager.contactCount());
+//            Log.d("CONTACTS", "::::: " + contactList.toString());
+//            Log.d("CONTACT COUNT", "::::: " + dbManager.contactCount());
         }
 
     }
---
+++ app/src/main/java/com/istef/sqlite_contactmanager/adapter/MainRecViewAdapter.java
@@ -0,0 +1,64 @@
+package com.istef.sqlite_contactmanager.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.istef.sqlite_contactmanager.R;
+import com.istef.sqlite_contactmanager.model.Contact;
+
+import java.util.List;
+
+
+public class MainRecViewAdapter extends RecyclerView.Adapter<MainRecViewAdapter.ViewHolder> {
+    private Context context;
+    private List<Contact> contactList;
+
+
+    public MainRecViewAdapter(Context context, List<Contact> contactList) {
+        this.context = context;
+        this.contactList = contactList;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.contact_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Contact contact = contactList.get(position);
+
+        holder.contactName.setText(contact.getName());
+        holder.contactPhone.setText(contact.getPhoneNumber());
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView contactName;
+        public TextView contactPhone;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            contactName = itemView.findViewById(R.id.txtName);
+            contactPhone = itemView.findViewById(R.id.txtPhone);
+
+        }
+    }
+}
